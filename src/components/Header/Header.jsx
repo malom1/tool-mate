b136ed74e91257ab489a221e9a2fba70564b6a9b
Mohammad Alom <malom1@fordham.edu>
2025-04-05 20:26:18 -0400

    add header orientation styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,12 +4,12 @@ export default function Header() {
 
     return(
         <>
-            <div className="header-container">
+            <div className="header">
                 <div className="logo">
                     <img src="./assets/logo.png" alt="" />
                     <h1>Tool Mate</h1>
                 </div>
-                <div className="user-container">
+                <div className="user-info">
                     <h4>John Doe</h4>
                     <p>Aircraft Maintenance Technician</p>
                 </div>
