40439e3f70556da704017ff2d17554966616d985
Mohammad Alom <malom1@fordham.edu>
2025-04-09 20:39:27 -0400

    add function to handle active sign in and display in DOM

--- src/pages/Tools/Tools.jsx
+++ src/pages/Tools/Tools.jsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 
 export default function Tools () {
 
-    // const [activeSignIn, setActiveSignIn] = useState([])
+    const [activeSignIn, setActiveSignIn] = useState([])
 
     const [inputs, setInputs] = useState({
         name: "",
@@ -15,12 +15,25 @@ export default function Tools () {
         setInputs((prev) => ({...prev, [name]: value}));
     }
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
+        const time = new Date().toLocaleString();
+
+        if(!inputs.name || !inputs.id || !inputs.tool){
+            alert("Enter all fields")
+            return;
+        }
+
+        setActiveSignIn([...activeSignIn, {...inputs, time}]);
+
+        setInputs({name:"", id:"", tool: ""});
+    }
 
     return (
         <div>
             <h1>Tool Management</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>
                     Name:
                     <input 
@@ -52,6 +65,22 @@ export default function Tools () {
                 </label>
                 <button type="submit">submit</button>
             </form>
+            <h2>Active Tool Sign Ins</h2>
+            <ul>
+                {activeSignIn.length > 0 ? (
+                    activeSignIn.map((entry, index) =>
+                        <li key={index}>
+                            <strong>{entry.tool}</strong> - {entry.name} ({entry.id})
+                            <br />
+                            <small>Signed in at: {entry.time}</small>
+                            <button>Sign Out</button>
+                        </li>
+                    )
+                ) : (
+                    <p>No active sign ins</p>
+                )
+                }
+            </ul>
         </div>
     )
 }
