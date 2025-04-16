573a78bf4a6490b6c4d36d6715e59374c5a8f07d
Mohammad Alom <malom1@fordham.edu>
2025-04-16 19:22:50 -0400

    handle session checking

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,37 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { supabase } from './supabaseClient'
 import './App.css'
+
 import Header from './components/Header/Header'
 import Sidebar from './components/Sidebar/Sidebar'
 import Tools from './pages/Tools/Tools'
 import Consumables from './pages/Consumables/Consumables'
 import Vehicles from './pages/Vehicles/Vehicles'
 import Dashboard from './pages/Dashboard/Dashboard'
+import Auth from './pages/Auth/Auth'
 
 function App() {
+
+  const [session, setSession] = useState(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: {session}}) => {
+      setSession(session)
+    });
+
+    const { data: listener } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    });
+
+    return () => {
+      listener.subscription.unsubscribe();
+    };
+  }, []);
+
+  if(!session) {
+    return <Auth />
+  }
 
   return (
     <Router>
