f256e939ab1afe4d46d4f38e5f2695835b8918e6
Mohammad Alom <malom1@fordham.edu>
2025-04-18 21:47:30 -0400

    create memo component

---
+++ src/components/Memo/Memo.jsx
@@ -0,0 +1,20 @@
+import './Memo.css'
+
+export default function Memo() {
+
+    return(
+        <div className="container">
+            <h2 className='memo-header'>Weekly Memo</h2>
+            <div className="memo">
+                <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut eos nobis saepe eius omnis vel est quis ea laudantium culpa, ab eaque, ut eligendi obcaecati quas fugiat voluptatibus asperiores explicabo?
+                </p>
+            </div>
+            <div className="memo">
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint quae illo itaque. Excepturi quis consequatur dolorum ex sit? Temporibus, ratione ab laborum voluptatibus illum corrupti quas totam deserunt dignissimos fugit.
+                </p>
+            </div>
+        </div>
+    )
+}
--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,14 +1,18 @@
 import DateTime from "../../components/DateTime/DateTime"
 import styles from "./Dashboard.module.css"
+import Memo from "../../components/Memo/Memo"
 
 export default function Dashboard () {
 
     return (
-        <div className="main-container">
-            <h1>Dashboard</h1>
-            <DateTime className={styles.dateTime}/>
-            <h3 className={styles.welcome}>Welcome to the Tool Mate Dashboard!</h3>
-            <p>Select the options on the left sidebar to start logging.</p>
+        <div className={styles.container}>
+            <div className={styles.dashboard}>
+                <h1>Dashboard</h1>
+                <DateTime className={styles.dateTime}/>
+                <h3 className={styles.welcome}>Welcome to the Tool Mate Dashboard!</h3>
+                <p>Select the options on the left sidebar to start logging.</p>
+            </div>
+            <Memo className={styles.memo}/>
         </div>
 
     )
